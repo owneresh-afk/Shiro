69f9ef97c342c6e5c1f8c71dfe4c7eebc740ccc6
owneresh-afk <owneresh@gmail.com>
2026-05-08 16:40:37 +0530

    Create stripeapi.py

---
+++ stripeapi.py
@@ -0,0 +1,6 @@
+async def try_checkout_card(card_data, proxy=None):
+    return {"status": "error", "msg": "Stripe module not yet configured"}
+
+async def fetch_checkout_info(site_url):
+    return None
+  
