3d3bcab53cd38b07775d37d10f7f9ea774719ccb
owneresh-afk <owneresh@gmail.com>
2026-05-08 16:41:29 +0530

    Create braintreeapi.py

---
+++ braintreeapi.py
@@ -0,0 +1,6 @@
+async def run_braintree_check_sync(card_data, site, proxy=None):
+    return "Braintree module not yet configured"
+
+async def check_bt_site_fast(site_url, proxy=None):
+    return False
+  
