757248a4492f61ced37e2496f87d787946a95ce3
owneresh-afk <owneresh@gmail.com>
2026-05-08 16:42:15 +0530

    Create stripecharge.py

---
+++ stripecharge.py
@@ -0,0 +1,6 @@
+async def check_stripe_gate(card_data, gate_url, proxy=None):
+    return "Stripe Charge module not yet configured"
+
+def format_stripe_ui(result):
+    return "Result: " + str(result)
+  
